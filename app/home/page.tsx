15bc70b0b4d71581512c783d870ea1fff1d53bea
Niluminda007 <princeofpersiatwathrones@gmail.com>
2024-01-30 17:19:40 +0200

    added images to the products

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,12 +1,46 @@
 "use client";
 
 import CategoryBar from "@/components/Home/CategoryBar";
-import React, { useEffect } from "react";
+import { ProductType } from "@/types/types";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = () => {
+  const [products, setProducts] = useState<ProductType[]>([]);
+
+  const fetchProducts = async () => {
+    try {
+      const { data } = await axios.post("/api/fetch-products-category", {
+        categoryName: "Spices",
+      });
+      setProducts(data);
+    } catch (error) {
+      console.error("Error fetching products", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  console.log(products);
   return (
     <div className="w-full h-full flex flex-col px-16 py-8 ">
       <CategoryBar />
+
+      <div className="flex flex-wrap gap-4">
+        {products &&
+          products.map((product) => (
+            <div
+              className="w-32 h-32"
+              key={product.id}
+              style={{
+                backgroundImage: `url('${product.images[0].url}')`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+            ></div>
+          ))}
+      </div>
     </div>
   );
 };
