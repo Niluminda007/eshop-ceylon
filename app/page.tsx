3bd923b022a358e93f9b26db9de3bf0ad7560dc5
Niluminda007 <princeofpersiatwathrones@gmail.com>
2024-02-02 21:39:06 +0200

    made minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,14 @@ import HomePage from "@/components/Home/HomePage";
 import { useState, useEffect } from "react";
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  // useEffect(() => {
-  //   const loadingTimeOut = setTimeout(() => {
-  //     setIsLoading(false);
-  //   }, 3500);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  useEffect(() => {
+    const loadingTimeOut = setTimeout(() => {
+      setIsLoading(false);
+    }, 3500);
 
-  //   return () => clearTimeout(loadingTimeOut);
-  // }, []);
+    return () => clearTimeout(loadingTimeOut);
+  }, []);
 
   return <>{isLoading ? <LoadingScreen /> : <HomePage />}</>;
 };
